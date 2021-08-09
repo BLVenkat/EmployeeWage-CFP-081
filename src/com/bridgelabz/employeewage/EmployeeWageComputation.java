0d2e686e5de664937d4c22ece782252a0dbeeb4c
BLVenkat <venkatreddy.mulaa@gmail.com>
2021-08-09 12:21:41 +0530

    [venkat] : Refactored employee daily wage calculation feature implemented

--- src/com/bridgelabz/employeewage/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewage/EmployeeWageComputation.java
@@ -3,17 +3,19 @@ package com.bridgelabz.employeewage;
 public class EmployeeWageComputation {
 
 	public static final int IS_PRESENT = 1;
+	public static final int WAGE_PER_HOUR = 20;
 	
 	public static void main(String[] args) {
 		
+		int empHrs=0;
+		int empWage=0;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		
 		if(empCheck == IS_PRESENT) {
-			System.out.println("Employee Is Present");
+			empHrs=8;
 		}
-		else {
-			System.out.println("Employee is absent");
-		}
+		empWage = empHrs*WAGE_PER_HOUR;
+		System.out.println("Employee Daily Wage: " +empWage);
 
 	}
 	
