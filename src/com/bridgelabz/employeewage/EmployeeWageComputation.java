7e20975844c70d3fe708d86031ff74081d1102f0
BLVenkat <venkatreddy.mulaa@gmail.com>
2021-08-09 12:39:10 +0530

    [venkat] : Refactored part time feature implemented

--- src/com/bridgelabz/employeewage/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewage/EmployeeWageComputation.java
@@ -2,18 +2,22 @@ package com.bridgelabz.employeewage;
 
 public class EmployeeWageComputation {
 
-	public static final int IS_PRESENT = 1;
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
 	public static final int WAGE_PER_HOUR = 20;
 	
 	public static void main(String[] args) {
 		
 		int empHrs=0;
-		int empWage=0;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		int empWage;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		
-		if(empCheck == IS_PRESENT) {
+		if(empCheck == IS_FULL_TIME) 
 			empHrs=8;
-		}
+		
+		else if (empCheck == IS_PART_TIME) 
+			empHrs=4;
+		
 		empWage = empHrs*WAGE_PER_HOUR;
 		System.out.println("Employee Daily Wage: " +empWage);
 
