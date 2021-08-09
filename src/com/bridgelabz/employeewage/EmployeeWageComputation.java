a282ec4fded478feeb91d33103a3b9204dcb18a6
BLVenkat <venkatreddy.mulaa@gmail.com>
2021-08-09 12:03:10 +0530

    [venkat] : Added Employee attendance feature implemented

--- src/com/bridgelabz/employeewage/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewage/EmployeeWageComputation.java
@@ -2,7 +2,20 @@ package com.bridgelabz.employeewage;
 
 public class EmployeeWageComputation {
 
+	public static final int IS_PRESENT = 1;
+	
 	public static void main(String[] args) {
 		
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		
+		if(empCheck == IS_PRESENT) {
+			System.out.println("Employee Is Present");
+		}
+		else {
+			System.out.println("Employee is absent");
+		}
+
 	}
+	
+	
 }
